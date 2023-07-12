36a4db57e024d46005e2dc92ec09d1b58789bd8c
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-12 15:02:15 +0000

    group

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
   get '/search', to: 'searches#search'
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
     resource :group_users, only: [:create, :destroy]
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
 end
