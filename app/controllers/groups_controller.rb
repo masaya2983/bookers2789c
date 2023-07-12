71305e23b894ccd9f72cfcd8b2f836431848a48c
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-12 14:55:31 +0000

    group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -38,7 +38,17 @@ before_action :authenticate_user!
         render "edit"
       end
     end
-  
+  def new_mail
+   @group = Group.find(params[:group_id])
+  end
+
+  def send_mail
+    @group = Group.find(params[:group_id])
+    group_users = @group.users
+    @mail_title = params[:mail_title]
+    @mail_content = params[:mail_content]
+    ContactMailer.send_mail(@mail_title, @mail_content,group_users).deliver
+  end
     private
   
     def group_params
@@ -51,6 +61,6 @@ before_action :authenticate_user!
         redirect_to groups_path
       end
     end
-  end
+  
 
 end
